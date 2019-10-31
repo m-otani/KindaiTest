f607711083ae9211c3b53e0ef1f980f550242298
m-otani <m-otani@i.kyoto-u.ac.jp>
2019-10-31 12:11:36 +0900

    test commit 2

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+def main():
+    print("Hello world")
+
+def test_main():
+    a = main()
+    b = "Hello world"
+    assert a == b
+
+# main関数呼び出し
+if __name__ == "__main__":
+    main()
